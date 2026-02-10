22db08aed1478f7d3b36e15a44add97e1491e1ee
Renaat Debleu <rdebleu@eWallah.net>
2026-02-10 15:07:06 +0000

    code_review

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -40,10 +40,10 @@ use stdClass;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-    core_userlist_provider,
-    paygw_provider,
     \core_privacy\local\metadata\provider,
-    \core_privacy\local\request\plugin\provider {
+    \core_privacy\local\request\plugin\provider,
+    core_userlist_provider,
+    paygw_provider {
     /**
      * Returns meta data about this system.
      *
